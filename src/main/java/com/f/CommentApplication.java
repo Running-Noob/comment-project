5747364e7883775efbe05d2bd4e1f4b5c423c131
Running-Noob <fang_zhuoyue@163.com>
2024-03-05 20:52:23 +0800

    remove out/

--- src/main/java/com/f/CommentApplication.java
+++ src/main/java/com/f/CommentApplication.java
@@ -4,8 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.f.mapper")
 @SpringBootApplication
+@MapperScan("com.f.mapper")
 public class CommentApplication {
 
     public static void main(String[] args) {
